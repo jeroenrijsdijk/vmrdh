f41cd843908b9fb4e4684869c45ba071bbb8dd09
Jeroen Rijsdijk <jeroenrijsdijk@users.noreply.github.com>
2018-01-18 08:24:17 +0100

    Update ABA-stap1-MaakSkim.rb

--- ABA/ABA-stap1-MaakSkim.rb
+++ ABA/ABA-stap1-MaakSkim.rb
@@ -27,7 +27,7 @@ toedelenAutoRdAon	= false
 toedelenOV		= true
 toedelenFiets		= false
 
-aantalThreads	= 8	#nr of threads while 
+aantalThreads	= 8	# max aantal threads tijdens toedelen
 
 if toedelenVracht
 	writeln "   * vrachtautoverkeer"
@@ -65,35 +65,34 @@ end
 
 if toedelenAutoSpits
   writeln "   * personenauto's"
-
   for t in [2,3] 
-  traffic							= OtTraffic.new
-  traffic.assignMethod			= VOLUMEAVERAGING
-  traffic.iterations				= 20
-  traffic.junctions 				= true
-  traffic.junctionParameters 		= [0.5,1.0]
-  #traffic.junctionVersion 		= 20
-  traffic.epsilon					= 0.00000001
-  traffic.functionType			= 19
-  traffic.bprPerType=[[[1..14,71..73],[0.5,4.0]], \
-       [[20,21,23,25,62,68..70,74,75],[1.0,4.0]], \
-       [[22,24,26..28,35..39,42,63,65],[1.5,4.0]], \
-       [[40,41,64,66],[2.0,4.0]], \
-       [[51,67],[4.0,4.0]], \
-       [[52,53,55..58],[0.0,4.0]]]
-  traffic.network			= [Auto,t]
-  traffic.routeFactors 			= [AutoCostAfstand,AutoCostTijd,0,0]
-  traffic.odMatrix			= [Totaal,Auto,t,103]
-  traffic.load				= [Totaal,Auto,t,103,Va,20]
-  traffic.preLoad			= [Totaal,Vracht,t,Usercat,Pae,1]
-  traffic.numberOfThreads		= aantalThreads
+	traffic							= OtTraffic.new
+	traffic.assignMethod			= VOLUMEAVERAGING
+	traffic.iterations				= 20
+	traffic.junctions 				= true
+	traffic.junctionParameters 		= [0.5,1.0]
+	#traffic.junctionVersion 		= 20
+	traffic.epsilon					= 0.00000001
+	traffic.functionType			= 19
+	traffic.bprPerType=[[[1..14,71..73],[0.5,4.0]], \
+	[[20,21,23,25,62,68..70,74,75],[1.0,4.0]], \
+	[[22,24,26..28,35..39,42,63,65],[1.5,4.0]], \
+	[[40,41,64,66],[2.0,4.0]], \
+	[[51,67],[4.0,4.0]], \
+	[[52,53,55..58],[0.0,4.0]]]
+	traffic.network			= [Auto,t]
+	traffic.routeFactors 			= [AutoCostAfstand,AutoCostTijd,0,0]
+	traffic.odMatrix			= [Totaal,Auto,t,103]
+	traffic.load				= [Totaal,Auto,t,103,Va,20]
+	traffic.preLoad			= [Totaal,Vracht,t,Usercat,Pae,1]
+	traffic.numberOfThreads		= aantalThreads
 
-  ################################################################################################
-  traffic.skimMatrix = [Totaal,Auto,t,3,[Cost,Afstand,Tijd],1]
-  traffic.skimFactors = [1.0,1.0,60.0]
-  ################################################################################################	
+	################################################################################################
+	traffic.skimMatrix = [Totaal,Auto,t,3,[Cost,Afstand,Tijd],1]
+	traffic.skimFactors = [1.0,1.0,60.0]
+	################################################################################################	
 
-  traffic.execute
+	traffic.execute
   end
 end
 
@@ -147,23 +146,23 @@ end
 
 if toedelenOV 
   for t in [1,2,3]
-  transit=OtTransit.new
-  transit.network 			= [Ov,t]
-  transit.logitParameters		= [0.5,0.1,0.1]
-  transit.load				= [Totaal,Ov,t,Usercat,Aon,1]
-  transit.routeFactors			= [[0,1,60,60,60,1]]
-  transit.minProbability 		= [0.02,0.02]
-  transit.minFind 			= [[Lopen,1]]                   
-  transit.searchRadius 			= [[Lopen,2.00]]       
-  transit.maxInterchanges 		= 5
-  transit.numberOfThreads		= aantalThreads
+	transit=OtTransit.new
+	transit.network 			= [Ov,t]
+	transit.logitParameters		= [0.5,0.1,0.1]
+	transit.load				= [Totaal,Ov,t,Usercat,Aon,1]
+	transit.routeFactors			= [[0,1,60,60,60,1]]
+	transit.minProbability 		= [0.02,0.02]
+	transit.minFind 			= [[Lopen,1]]                   
+	transit.searchRadius 			= [[Lopen,2.00]]       
+	transit.maxInterchanges 		= 5
+	transit.numberOfThreads		= aantalThreads
 
 
-  ####################################################################
-  transit.skimMatrix=[1,4,t,3,[1,2,3,4,5,6,7],1]
-  ####################################################################
+	####################################################################
+	transit.skimMatrix=[1,4,t,3,[1,2,3,4,5,6,7],1]
+	####################################################################
 
-  transit.execute
+	transit.execute
   end
 end
  
@@ -175,7 +174,7 @@ if toedelenFiets
   traffic.numberOfThreads			= aantalThreads
 
   ####################################################################
-  traffic.skimMatrix = [Totaal,Fiets,t,3,[Cost,Afstand,Tijd],1]
+  traffic.skimMatrix = [Totaal,Fiets,t,3,[Cost,Afstand,Tijd],1]    # nog testen of dit juist is
   traffic.skimFactors = [1.0,1.0,60.0]
   ####################################################################
 
