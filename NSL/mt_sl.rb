e3da02a5cb9cc40448d2365d1bb880ef5f385f78
Jeroen Rijsdijk <jeroenrijsdijk@users.noreply.github.com>
2018-03-01 10:21:41 +0100

    Update mt_sl.rb

--- NSL/mt_sl.rb
+++ NSL/mt_sl.rb
@@ -1,47 +1,55 @@
 # OmniTRANS Job for 'VMRDH1_NSL2018'
 # Created 28-2-2018 15:35:16
 #
-# calc a new 2017 matrix for NSL
-#
+# Calc a new 2017 matrix for NSL
+# Maastunnel Selected Link - 378,2 (Noord -> Zuid)
 # jri 02/2018
 #
+# Dit script gebruikt de selectedlink matix in result 41 
+#     in variant "2017"
 #
-#
-
 #####################
 # pmturi's for AUTO and VRACHT
 #####################
-mats=[[1,3,1,3],[1,3,2,3],[1,3,3,3],[1,2,1,103],[1,2,2,101],[1,2,3,103]]
+mats=[[1,3,1,1],[1,3,2,1],[1,3,3,1],[1,2,1,103],[1,2,2,101],[1,2,3,103]]
 slmats=[  
-[1,3,1,41, 1, 1, 378,2 ], \
-[1,3,2,41, 1, 1, 378,2 ], \
-[1,3,3,41, 1, 1, 378,2 ], \
-[1,2,1,41,11,20, 378,2 ], \
-[1,2,2,41,11,20, 378,2 ], \
-[1,2,3,41,11,20, 378,2 ]]
+[1,3,1,41,10,1], \
+[1,3,2,41,10,1], \
+[1,3,3,41,10,1], \
+[1,2,1,41,11,20], \
+[1,2,2,41,11,20], \
+[1,2,3,41,11,20]]
 
 #####################
 # open the cubes
 #####################
-mc=OtMatrixcube.open("2017_SMC")
-ms=OtSelectedLinkCube.open("")
+mc=OtMatrixCube.open("2017_SMC")
+ms=OtSelectedLinkCube.open("2017")
 
 
 #####################
 # open the new cube, or create it
 #####################
 begin
-  mcn=OtMatrixcube.open("2017_SMC_MT")
+  mcn=OtMatrixCube.open("2017_SMC_MT")
 rescue
-  mcn=OtMatrixcube.create("2017_SMC_MT")
+  mcn=OtMatrixCube.create("2017_SMC_MT")
 end
+
 
 #####################
 # New matrix 
 #####################
 mats.each_index {  |i|
-  smc=mc[*mats[i]]   # Get the matrix
-  slm=ms[*slmats[i]] # Get the selected link matrix
-  smc=smc-slm*0.5    # now substract half of the Selected Link Matrix
-  msn[*mats[i]]=smc   # put it into the new cube
+  print(*mats[i].join("-"),"\n")
+  
+  # smc=mc[*mats[i]]   # is the smc matrix
+  # slm=ms[*slmats[i]] # is the selected link matrix  
+  # substract half of the Selected Link Matrix
+  # like smc = smc - slm.multiply!(0.5)
+  # then put it into the new cube
+  
+  mcn[*mats[i]]=mc[*mats[i]]-ms[*slmats[i]].multiply!(0.5)  
 }
+
+print "Einde script.\n"
