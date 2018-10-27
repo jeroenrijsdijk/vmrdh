8b0a21a702e6793ff3d4a817888fc842e2610d9d
Jeroen Rijsdijk <jeroenrijsdijk@users.noreply.github.com>
2018-10-27 10:33:00 +0200

    Update groeifactormodule.rb

--- Groeifactor/groeifactormodule.rb
+++ Groeifactor/groeifactormodule.rb
@@ -29,7 +29,7 @@ def iter(pmtu, todocols, todorows)
   
 
   puts("Init...")
-  my_old_matrix1 = my_matrix_cube[*pmtu]
+  #my_old_matrix1 = my_matrix_cube[*pmtu]
 
   #inititaliseer de Groeifactor methode
   my_run1 = OtGrowthFactor.new
@@ -47,7 +47,7 @@ def iter(pmtu, todocols, todorows)
   }
 
   puts("Run...")
-  my_growth_matrix = my_run1.execute(my_old_matrix1, rows1, cols1)
+  my_growth_matrix = my_run1.execute(my_matrix_cube[*pmtu], rows1, cols1)
   my_newmatrix_cube[*pmtu] = my_growth_matrix
   
   ####################################################################
@@ -66,7 +66,13 @@ VrOS   = [1,3,1,1]
 VrRD   = [1,3,2,1]
 VrAS   = [1,3,3,1]
 
-# In zone 134 rijden mode=Auto, tijd=ochtendspits 234 auto's (coltotal) weg en er komen er 123 aan (rowtotals). 
+# Voorbeeld: 
+#   r = iter(AutoOS, [[134, 123], .. ], [[134, 234], .. ]
+# In zone 134 
+#  mode=Auto, tijd=ochtendspits 
+#   234 auto's komen aan (aankomsten, coltotal), en 
+#   123 auto's gaan weg (vertrekken, rowtotals). 
+#
 r = iter(AutoOS, [[134, 123],[135, 456],[136, 789]], [[134, 234],[135, 456],[136, 789]])
 
 r = iter(AutoRD, [[134, 123],[135, 456],[136, 789]], [[134, 123],[135, 456],[136, 789]])
